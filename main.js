c1d1109bc3745120413f54c5e646716eff1bf2d0
Pallavirs0305 <pallaviraghu1213@gmail.com>
2026-02-28 01:00:16 +0530

    main.js

--- main.js
+++ main.js
@@ -3,6 +3,40 @@ const ctx = canvas.getContext("2d");
 
 let gameState = "intro";
 
+const player = {
+  x: 100,
+  y: 380,
+  width: 40,
+  height: 40,
+  speed: 4
+};
+
+const keys = {};
+
+document.addEventListener("keydown", e => {
+  keys[e.key] = true;
+
+  if (gameState === "intro" && e.key === "Enter") {
+    gameState = "explore";
+  }
+});
+
+document.addEventListener("keyup", e => {
+  keys[e.key] = false;
+});
+
+function update() {
+  if (gameState === "explore") {
+    if (keys["ArrowLeft"]) player.x -= player.speed;
+    if (keys["ArrowRight"]) player.x += player.speed;
+
+    // Keep inside screen
+    if (player.x < 0) player.x = 0;
+    if (player.x + player.width > canvas.width)
+      player.x = canvas.width - player.width;
+  }
+}
+
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
@@ -12,15 +46,20 @@ function draw() {
     ctx.fillText("The Trail of Wild Roses", 250, 200);
     ctx.fillText("Press Enter", 360, 250);
   }
+
+  if (gameState === "explore") {
+    // Background
+    ctx.fillStyle = "#1c1c1c";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    // Player (Nick placeholder)
+    ctx.fillStyle = "orange";
+    ctx.fillRect(player.x, player.y, player.width, player.height);
+  }
 }
 
-document.addEventListener("keydown", e => {
-  if (e.key === "Enter") {
-    gameState = "explore";
-  }
-});
-
 function gameLoop() {
+  update();
   draw();
   requestAnimationFrame(gameLoop);
 }
