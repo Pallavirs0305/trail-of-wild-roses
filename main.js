53bf79ba9781e66cf0c25f253ed37c53eaf7b64e
Pallavirs0305 <pallaviraghu1213@gmail.com>
2026-02-28 09:31:30 +0530

    main.js

--- main.js
+++ main.js
@@ -33,7 +33,7 @@ window.onload = function () {
     keys[e.key] = false;
   });
 
-  function update() {
+function update(deltaTime) {
     if (gameState === "explore") {
       if (keys["ArrowLeft"]) player.x -= player.speed;
       if (keys["ArrowRight"]) player.x += player.speed;
@@ -53,40 +53,73 @@ window.onload = function () {
       ctx.fillText("The Trail of Wild Roses", 250, 200);
       ctx.fillText("Press Enter", 360, 250);
     }
+if (gameState === "explore") {
 
-    if (gameState === "explore") {
+  // Background
+  ctx.fillStyle = "#1c1c1c";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-      ctx.fillStyle = "#1c1c1c";
-      ctx.fillRect(0, 0, canvas.width, canvas.height);
+  // --- FOX ---
+  let row = 0;
 
-      // Draw fox (player)
-      ctx.drawImage(
-        foxImage,
-        0, 0, 48, 48,
-        player.x,
-        player.y,
-        48,
-        48
-      );
+  if (direction === "right") row = 1;
+  if (direction === "left") row = 2;
+  if (direction === "idle") row = 0;
 
-      // Draw rabbit
-      ctx.drawImage(
-        rabbitImage,
-        0, 0, 48, 48,
-        700,
-        380,
-        48,
-        48
-      );
-    }
+  ctx.drawImage(
+    foxImage,
+    currentFrame * 48,
+    row * 48,
+    48,
+    48,
+    player.x,
+    player.y,
+    48,
+    48
+  );
+
+  // --- RABBIT ---
+  ctx.drawImage(
+    rabbitImage,
+    rabbitFrame * 48,
+    0,
+    48,
+    48,
+    700,
+    380,
+    48,
+    48
+  );
+
+  // --- HEART ---
+  if (nearRabbit) {
+
+    ctx.save();
+
+    ctx.translate(724, 350);
+    ctx.scale(heartScale, heartScale);
+
+    ctx.fillStyle = "pink";
+
+    ctx.beginPath();
+    ctx.moveTo(0, 0);
+    ctx.bezierCurveTo(-10, -10, -20, 10, 0, 20);
+    ctx.bezierCurveTo(20, 10, 10, -10, 0, 0);
+    ctx.fill();
+
+    ctx.restore();
   }
+}
+    
+function gameLoop(timestamp) {
 
-  function gameLoop() {
-    update();
-    draw();
-    requestAnimationFrame(gameLoop);
-  }
+  let deltaTime = timestamp - lastTime;
+  lastTime = timestamp;
 
-  gameLoop();
+  update(deltaTime);
+  draw();
 
-};
+  requestAnimationFrame(gameLoop);
+}
+
+gameLoop(0);
